67ecac83905b762d76225d61117482a5f73b7028
Kotwade <kotwade@yandex.ru>
2021-07-09 13:36:16 +0500

    Добавляет реализацию попапа

--- build/js/script.js
+++ build/js/script.js
@@ -29,3 +29,78 @@ const swiper = new Swiper('.swiper-container', {
     }
   }
 })
+
+//Попапы
+
+var ESC_KEY = 'Escape';
+
+var popup = document.querySelector('.cart-popup');
+var popupOpen = document.querySelector('.info__button');
+var popupClose = popup.querySelector('.cart-popup__button-cross');
+var overlay = document.querySelector('.overlay');
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === ESC_KEY) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  popup.classList.remove('cart-popup--hidden');
+  overlay.classList.remove('overlay--hidden');
+  pageBody.classList.add('page-body--opened');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  popup.classList.add('cart-popup--hidden');
+  overlay.classList.add('overlay--hidden');
+  pageBody.classList.remove('page-body--opened');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+popupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+popupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+overlay.addEventListener('click', function () {
+  popup.classList.add('cart-popup--hidden');
+  overlay.classList.add('overlay--hidden');
+});
+
+// Аккордеон
+
+var accordionItems = document.querySelectorAll('.accordion');
+var accordionPanes = document.querySelectorAll('.accordion__pane');
+
+var hidePane = function (button, pane) {
+  button.classList.add('accordion__toggle--inactive');
+  pane.classList.add('accordion__pane--hidden');
+};
+
+var showPane = function (button, pane) {
+  button.classList.remove('accordion__toggle--inactive');
+  pane.classList.remove('accordion__pane--hidden');
+};
+
+var toggleAccordion = function (evt) {
+  Array.prototype.forEach.call(accordionPanes, function (accordionPane) {
+    var button = accordionPane.closest('.accordion').querySelector('.accordion__toggle');
+    if (button === evt.target && !button.classList.contains('accordion__toggle--inactive') || button !== evt.target) {
+      hidePane(button, accordionPane);
+    } else if (button === evt.target) {
+      showPane(button, accordionPane);
+    }
+  });
+};
+
+Array.prototype.forEach.call(accordionItems, function (accordion) {
+  var accordionToggleButton = accordion.querySelector('.accordion__toggle');
+  var accordionPane = accordion.querySelector('.accordion__pane');
+  hidePane(accordionToggleButton, accordionPane);
+  accordionToggleButton.addEventListener('click', toggleAccordion);
+});
