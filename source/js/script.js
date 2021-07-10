8a82f0014f2fea3b72b40f042231e21fd56feffe
Kotwade <kotwade@yandex.ru>
2021-07-10 18:12:42 +0500

    Добавляет стрелки для слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -27,8 +27,12 @@ const swiper = new Swiper('.swiper-container', {
       slidesPerView: 4,
       spaceBetween: 40
     }
-  }
-})
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+});
 
 //Попапы
 
